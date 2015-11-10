373d05979871547d7d97588cfa8bda5c4ba388a1
qbchen <qbchen@corp.netease.com>
2015-11-11 00:16:15 +0800

    漏掉的

---
+++ src/main/java/ief/utils/SecretUtil.java
@@ -0,0 +1,102 @@
+package ief.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class SecretUtil {
+	
+	public static String PASSWORD="WeAre-IEF_Family";
+	public static String AUTHPASSWORD="IEF-auth_Pass#OK";
+	public static int LENGTH=128;
+	
+    /**
+     * 加密
+     * 
+     * @param content 需要加密的内容
+     * @param password  加密密码
+     * @return
+     */
+    public static String encrypt(String content,String password) {
+        try {           
+        	KeyGenerator kgen = KeyGenerator.getInstance("AES");  
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG" );   
+            secureRandom.setSeed(password.getBytes());   
+		    kgen.init(LENGTH, secureRandom);  
+		    SecretKey secretKey = kgen.generateKey();  
+		    byte[] enCodeFormat = secretKey.getEncoded();  
+		    SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");  
+		    Cipher cipher = Cipher.getInstance("AES");// 创建密码器  
+		    byte[] byteContent = content.getBytes("utf-8");  
+		    cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化  
+		    byte[] result = cipher.doFinal(byteContent);  
+            return Base64.encodeBase64String(result); // 加密
+        } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+                e.printStackTrace();
+        } catch (InvalidKeyException e) {
+                e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+                e.printStackTrace();
+        } catch (BadPaddingException e) {
+                e.printStackTrace();
+        }
+        return null;
+    }
+    
+    /**解密
+     * @param content  待解密内容
+     * @param password 解密密钥
+     * @return
+     */
+    public static String decrypt(String content,String password) {
+        try {
+        	KeyGenerator kgen = KeyGenerator.getInstance("AES");  
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG" );   
+            secureRandom.setSeed(password.getBytes());   
+		   kgen.init(LENGTH, secureRandom);  
+		   SecretKey secretKey = kgen.generateKey();  
+		   byte[] enCodeFormat = secretKey.getEncoded();  
+		   SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");  
+		   Cipher cipher = Cipher.getInstance("AES");// 创建密码器  
+		   cipher.init(Cipher.DECRYPT_MODE, key);// 初始化  
+		   byte[] result = cipher.doFinal(Base64.decodeBase64(content));
+            return new String(result); // 加密
+        } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+                e.printStackTrace();
+        } catch (InvalidKeyException e) {
+                e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+                e.printStackTrace();
+        } catch (BadPaddingException e) {
+                e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static void main(String[] args) {
+    	long start1=System.currentTimeMillis();
+    	String result=SecretUtil.encrypt("test22_Et*",SecretUtil.PASSWORD);
+    	long start2=System.currentTimeMillis();
+    	System.out.println(result);
+    	System.out.println(SecretUtil.decrypt(result,SecretUtil.PASSWORD));
+    	long start3=System.currentTimeMillis();
+    	System.out.println((start2-start1)+"\t"+(start3-start2));
+	}
+}
